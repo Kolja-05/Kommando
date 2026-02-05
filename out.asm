8e34a5d77350cbacb4ce01cee3f47dfa33f480bd
Kolja-05 <kolja.stelzig@gmx.de>
2026-02-05 13:03:07 +0100

    removing wrong file

--- out.asm
+++ out.asm
@@ -8,6 +8,14 @@ mov rax, 3
 pop rbx
 add rax, rbx
 mov QWORD [rbp-8], rax
+mov rax, 1
+mov QWORD [rbp-16], rax
+mov rax, QWORD [rbp-8]
+push rax
+mov rax, QWORD [rbp-16]
+pop rbx
+add rax, rbx
+mov QWORD [rbp-24], rax
 mov rax, QWORD [rbp-8]
 mov rdi, rax
 mov rax, 60
